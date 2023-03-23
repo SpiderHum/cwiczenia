2ec09cc1ac0252a71a5eaabe6e3d1e87a77853b5
SpiderHum <szymonmars@gazeta.pl>
2023-03-23 12:11:44 +0100

    notatki

---
+++ notatki.py
@@ -0,0 +1,6 @@
+#miejsce na notatki, przydatne linki, funkcje, etc. 
+
+#https://www.programiz.com/python-programming/methods/string/count
+amongus="Wczoraj bylam w zachodniej cwiartce"
+print(amongus.count("a"))
+#przydatne do wisielca
--- unit.py
+++ unit.py
@@ -1,2 +1,11 @@
 #projekt numer 5 -- konwenter jednostkek
 #Udajmy się do Trojej, ku spotkaniu ruin a pozostałości po straciu dziesięcioletnim tytanów
+'''
+km kwadratowy == 1 000 000 m2 == 100 ha == 10 000 ar
+metr kwadratowy == 100 dm2 == 10 000 cm2 == 1 000 000 mm2
+dm kwadratowy == 100 cm2 == 10 000 mm2
+cm kwadratowy == 100 mm2
+mm kwadratowy == 0,01 cm2
+hektar = 100 ar
+ar = 100 m2
+'''
