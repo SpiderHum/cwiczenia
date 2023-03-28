42a3b6a18672cedb94e48d4e59c5b65ca2d6fa4f
SpiderHum <szymonmars@gazeta.pl>
2023-03-28 12:14:20 +0200

    . \

---
+++ kamienpapier.py
@@ -0,0 +1,27 @@
+#dritte Projekt. Ein Stein, Ein Papier, Eine Schere. 
+import random
+i=random.randint(0,3) #output: 0, 1 lub 2, jkaby wyznacza punkt początkowy (0) i ile będzie liczyć (3)
+sys=("kamien", "papier", "nozyce")
+odp=sys(i) #coś tu mu nie gra. Sprawdź co.
+use=input("Hehe, zagrajmy w kamien papier nozyce. Ty zaczynaj.")
+print(odp)
+#kamień bije nozyczki
+#nozyczki bija papier
+#papier bije kamien
+
+if use==odp:
+    print("remis")
+elif use == "kamien" and odp == "nozyczki":
+    print("wygrales")
+elif use == "nozyczki" and odp == "papier":
+    print("wygrales")
+elif use == "papier" and odp == "kamien":
+    print("wygrales")
+elif odp == "kamien" and use == "nozyczki":
+    print("przegales")
+elif odp == "nozyczki" and use == "papier":
+    print("przegales")
+elif odp == "papier" and use == "kamien":
+    print("przegales")
+
+
