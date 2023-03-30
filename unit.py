2bf49c0946c36f20edab617098119b32d9846853
SpiderHum <szymonmars@gazeta.pl>
2023-03-30 12:00:40 +0200

    skończyłem kamienpapier

--- unit.py
+++ unit.py
@@ -9,3 +9,21 @@ mm kwadratowy == 0,01 cm2
 hektar = 100 ar
 ar = 100 m2
 '''
+m=int(input())
+km=0.001*m
+dm=10*m
+cm=100*m
+mm=1000*m
+print(f'Podales {m} metrow, jest to {km} kilometrow, {dm} decymetrow, {cm} centymetrów')
+
+#cal=25,4*0,1cm
+cal=2.54*cm #w centymetrach
+stopa=12*cal #w centymetrach
+jard=stopa*3
+pret=5,5*jard
+lancuch=4*pret
+furlong=10*lancuch
+mila=8*furlong
+liga=3*mila
+
+print(f'podałeś ')
